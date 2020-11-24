6c841d4df5d3fd77da5498f8ef623f7816ef3fae
screen <bao.future94@gmail.com>
2020-11-24 00:28:01 -0800

    finish homepage

--- js/main.js
+++ js/main.js
@@ -8,48 +8,24 @@ App = {};
     $("header .navbar ul.submenu")
       .removeClass("menu-mobile")
       .addClass(new_class);
-
-    // var heightPos1 = $('.pos1').height();
-    // $('.pos3').css({
-    //     'margin-top': -heightPos1 + 300 + 'px'
-    // });
   };
-  App.calculatorSidebarDetail = () => {
-    var $window = $(window);
-    var heightPos1 = $(".pos1").height();
-
-    var windowsize = $window.width();
-    if (windowsize >= 992) {
-      //if the window is greater than 440px wide then turn on jScrollPane..
-      $(".pos3").css({
-        "margin-top": -heightPos1 + 280 + "px",
-      });
-    } else if (windowsize < 992) {
-      $(".pos3").css({
-        "margin-top": 0 + "px",
-      });
-    } else {
-      return;
-    }
-  };
+  
 
   // Load First
  
   // When resize screen
   $(window).resize(function () {
     App.checkWindowSize();
-    App.calculatorSidebarDetail();
   });
 
   // Run first
   $(document).ready(function () {
-    $(document).ready(function() {
-      $('#example').DataTable( {
-          dom: 'Bfrtip',
-          buttons: [
-              'copy', 'csv', 'excel', 'pdf', 'print'
-          ]
-      } );
-  } );
+    App.checkWindowSize();
+
+    // Click menu mobile
+    $("header ul li").on("click", function () {
+      $(this).find("ul").toggleClass("active");
+      console.log(this);
+    });
   });
 })(jQuery);
